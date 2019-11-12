620d42865128029157ed7522ddb3cb888c6c2922
Miguelon26 <miguelandres2609@live.com.mx>
2019-11-11 21:42:42 -0600

    11/11/2019 Splash Screen añadido, pero lo hace en orden inverso.

--- app/src/main/java/com/example/rample/MainActivity.java
+++ app/src/main/java/com/example/rample/MainActivity.java
@@ -9,13 +9,28 @@ package com.example.rample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT = 4000;//ms para splash screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Handler e Intent para mostrar el splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, SplashScreenActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+
+    }//onCreate
+
+}//MainActivity
