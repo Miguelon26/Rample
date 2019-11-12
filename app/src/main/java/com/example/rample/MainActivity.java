2c033764281d6d05eed61d978d25c6cf92852b1a
Miguelon26 <miguelandres2609@live.com.mx>
2019-11-11 22:18:49 -0600

    11/11/2019 Se agregó el splash screen correctamente, también los íconos y el icon launcher.

--- app/src/main/java/com/example/rample/MainActivity.java
+++ app/src/main/java/com/example/rample/MainActivity.java
@@ -3,6 +3,7 @@
 * Jorge Luis Vasquez Osorio
 *
 * Proyecto para Aplicaciones de Tecnologia: Rample
+* MainActivity.java
 */
 
 package com.example.rample;
@@ -14,23 +15,11 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN_TIME_OUT = 4000;//ms para splash screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Handler e Intent para mostrar el splash screen
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, SplashScreenActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_SCREEN_TIME_OUT);
-
     }//onCreate
 
 }//MainActivity
--- app/src/main/java/com/example/rample/SplashScreenActivity.java
+++ app/src/main/java/com/example/rample/SplashScreenActivity.java
@@ -1,14 +1,35 @@
+/*
+ * Miguel Andres Izaguirre Valerio
+ * Jorge Luis Vasquez Osorio
+ *
+ * Proyecto para Aplicaciones de Tecnologia: Rample
+ * MainActivity.java
+ */
+
 package com.example.rample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT = 3000;//ms para splash screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Handler e Intent para mostrar el splash screen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
     }
 }
