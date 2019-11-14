25165314365fa1f059ce3e769a1a8c82e7bafc56
Miguelon26 <miguelandres2609@live.com.mx>
2019-11-13 18:35:40 -0600

    13/11/2019 Animación (bounce) al botón de Rample añadidos correctamente.

--- app/src/main/java/com/example/rample/MainActivity.java
+++ app/src/main/java/com/example/rample/MainActivity.java
@@ -9,17 +9,75 @@
 package com.example.rample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    Animation button_Animation;
+    ImageButton rample_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rample_Button = (ImageButton) findViewById(R.id.imageButton);
+
     }//onCreate
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        animateButton();
+    }//onStart
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        animateButton();
+    }//onResume
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }//onPause
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }//onStop
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }//onDestroy
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }//onRestart
+
+    /**********************************************************************************************/
+
+    //Al presionar el boton de Rample
+    public void didTapButton(View view) {
+        // Do something in response to button click
+        Log.d("RAMPLE", "Se ha presionado el boton.");
+
+    }//didTapButton
+
+    //Animar el boton de Rample
+    private void animateButton() {
+        button_Animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+        BounceInterpolator interpolator = new BounceInterpolator();
+        button_Animation.setInterpolator(interpolator);
+
+        rample_Button.startAnimation(button_Animation);
+    }//animateButton
 
 }//MainActivity
