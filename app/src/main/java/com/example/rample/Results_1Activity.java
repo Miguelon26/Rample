fbef00dd87a6d31e38f060b0ca5b8fb54f5f4866
Miguelon26 <miguelandres2609@live.com.mx>
2019-11-18 02:03:36 -0600

    18/11/2019 Se despliegan visualmente los datos de la película aleatoria. Se modificó la interfaz de usuario.

--- app/src/main/java/com/example/rample/Results_1Activity.java
+++ app/src/main/java/com/example/rample/Results_1Activity.java
@@ -1,36 +1,59 @@
 package com.example.rample;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonArray;
-import org.json.JSONArray;
-import org.json.JSONException;
+import com.squareup.picasso.Picasso;
+
 import org.json.JSONObject;
 
 public class Results_1Activity extends AppCompatActivity {
-    ImageButton rample_Button;
-    TextView enlaceRandom_TextView;
+    ImageButton rample_imageButton;
+    TextView loading_textView;
+    ImageView poster_imageView;
+    TextView title_textView;
+    TextView info_textView;
+    TextView trailer_textView;
+
+    String tempId = "";
+    boolean containsGenre = false, containsYear = false, containsRuntime = false, containsCertification = false, containsRating = false;
+
+    Handler handler = new Handler();
+    int apiDelay = 100;//ms
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results_1);
 
-        rample_Button = findViewById(R.id.imageButton2);
-        enlaceRandom_TextView = findViewById(R.id.resultados);
+        rample_imageButton = findViewById(R.id.rample_imageButton);
+        loading_textView = findViewById(R.id.loading_textView);
+        poster_imageView = findViewById(R.id.poster_imageView);
+        title_textView = findViewById(R.id.title_textView);
+        info_textView = findViewById(R.id.info_textView);
+        trailer_textView = findViewById(R.id.trailer_textView);
 
-        enlaceRandom_TextView.setText("Cargando tu experiencia...");
-        generateRandom();
+        handler.postDelayed(runnable = new Runnable() {
+            public void run() {
+                runRequest();
+                handler.postDelayed(runnable, apiDelay);
+            }
+        }, apiDelay);
 
     }//onCreate
 
@@ -69,30 +92,40 @@ public class Results_1Activity extends AppCompatActivity {
         // Do something in response to button click
         Log.d("RAMPLE", "Se ha presionado el boton.");
 
-        enlaceRandom_TextView.setText("Cargando tu experiencia...");
-        generateRandom();
+        loading_textView.setText("Cargando tu experiencia...");
+
+        handler.postDelayed(runnable = new Runnable() {
+            public void run() {
+                runRequest();
+                handler.postDelayed(runnable, apiDelay);
+            }
+        }, apiDelay);
+
+
+        //runRequest();
     }//didTapButton
 
-
-    private void generateRandom() {
+    private void runRequest() {
         String movieURL = "https://tv-v2.api-fetch.website/random/movie";
-        String showURL = "https://tv-v2.api-fetch.website/random/show";
 
-        //Peticion para pelicula aleatoria
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+
         JsonObjectRequest objectRequest = new JsonObjectRequest(
                 Request.Method.GET,
-                movieURL,//que URL esta pidiendo
+                movieURL,//URL
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {//respuesta
-                        Log.i("REST",response.toString());
+                        Log.i("REST", response.toString());
 
                         try {
-                            String title="", year ="",synopsis="",runtime="",trailer="",certification="",
-                            genres="",poster="";
-                            int rating=0;
+                            String id = "", title = "", year = "", synopsis = "", trailer = "", certification = "",
+                                    genres = "", poster = "";
+                            int runtime = 0, rating = 0;
+
+                            id = response.getString("_id");
+                            tempId = id;
 
                             if (response.has("title")) {
                                 title = response.getString("title");
@@ -104,14 +137,14 @@ public class Results_1Activity extends AppCompatActivity {
                                 synopsis = response.getString("synopsis");
                             }
                             if (response.has("runtime")) {
-                                runtime = response.getString("runtime");
+                                runtime = response.getInt("runtime");
                             }
                             if (response.has("trailer")) {
                                 trailer = response.getString("trailer");
-                            } else { trailer = "NA"; }
+                            }
                             if (response.has("certification")) {
                                 certification = response.getString("certification");
-                            } else { certification = "NA"; }
+                            }
                             if (response.has("genres")) {
                                 genres = response.getString("genres");
                             }
@@ -119,33 +152,73 @@ public class Results_1Activity extends AppCompatActivity {
                                 JSONObject images = response.getJSONObject("images");
                                 if (images.has("poster")) {
                                     poster = images.getString("poster");
-                                } else {poster="NA";}
+                                } else {
+                                    poster = "NA";
+                                }
                             }
                             if (response.has("rating")) {
                                 JSONObject percentage = response.getJSONObject("rating");
                                 if (percentage.has("percentage")) {
                                     rating = percentage.getInt("percentage");
-                                } else { rating = 0; }
+                                } else {
+                                    rating = 0;
+                                }
                             }
 
-                            enlaceRandom_TextView.setText("Título: "+title+"\nAño: "+year+"\nSinopsis: "+synopsis+
-                                    "\nDuración: "+runtime+" minutos\nTrailer: "+trailer+"\nClasificación: "+
-                                    certification+"\nGéneros: "+genres+"\nPoster: "+poster+"\nRating: "+rating);
-                        } catch (Exception e) { e.printStackTrace(); }
+                            //Obtener hasta 5 generos
+                            genres = genres.replaceAll("[\"\\[\\]]", "");
+                            String[] genre = genres.split(",");
+
+                            /**********************************************************************/
+
+                            //Resultado con filtros
+                            /*for (int i = 0; i < 5; i++) {
+                                if (genre[i].contains("drama")) {
+                                    containsGenre = true;
+                                } else {//loop
+                                    loading_textView.setText("Ahi va...");
+                                    //runRequest();
+                                }
+                            }//for*/
+
+                            //if (containsGenre == true) {
+                                //display
+                                loading_textView.setVisibility(View.GONE);
+
+                                poster = poster.substring(0, 4) + "s" + poster.substring(4);//subsrtr a https
+                                Picasso.get().load(poster).into(poster_imageView);
+                                title_textView.setText(Html.fromHtml("<br>" + title + "</b>"));
+                                info_textView.setText(Html.fromHtml("<b>Sinopsis: </b>" + synopsis + "\n<br><b>Año:</b> " + year + ".\n<br><b>Duración:</b> " + runtime + " minutos.\n" +
+                                        "<br><b>Géneros:</b> " + genre[0] + " " + genre[1] + " " + genre[2] + ".\n<br><b>Clasificación:</b> " + certification + "\n.<br><b>Rating:</b> " + rating + "/100."));
+                                trailer_textView.setText(Html.fromHtml("<b>Trailer:</b> " + trailer));
+
+                                handler.removeCallbacks(runnable);
+                                requestQueue.stop();
+
+
+                            //}
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.e("REST",error.toString());
+                        Log.e("RESPONSE", error.toString());
                     }
                 }
         );//requestQueue
 
         requestQueue.add(objectRequest);
         requestQueue.getCache().clear();
-    }//generateRandom
+
+    }//runRequest
 
 
 }
+
+
+
 
--- app/src/main/java/com/example/rample/SplashScreenActivity.java
+++ app/src/main/java/com/example/rample/SplashScreenActivity.java
@@ -11,16 +11,31 @@ package com.example.rample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_SCREEN_TIME_OUT = 1000;//ms para splash screen
+    TextView rample_textView;
+    ImageView rample_imageView;
+    ProgressBar progressBar;
+    private static int SPLASH_SCREEN_TIME_OUT = 1500;//ms para splash screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        rample_textView = findViewById(R.id.rample_textView);
+        rample_imageView = findViewById(R.id.rample_imageView);
+        progressBar = findViewById(R.id.progressBar);
+
+        progressBar.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBar.setVisibility(View.VISIBLE);
 
         //Handler e Intent para mostrar el splash screen
         new Handler().postDelayed(new Runnable() {
